5952c0e19c279284695504e1087121d049a62ba7
scott-gordon-devops <scott.gordon@slalom.com>
2023-07-24 14:10:06 -0400

    Init commit

---
+++ cdk_workshop/cdk_workshop_stack.py
@@ -0,0 +1,36 @@
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
+)
+
+from cdk_dynamo_table_view import TableViewer
+from .hitcounter import HitCounter
+
+class CdkWorkshopStack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.from_asset('lambda'),
+            handler='hello.handler',
+        )
+
+        hello_with_counter = HitCounter(
+            self, 'HelloHitCounter',
+            downstream=my_lambda
+        )
+
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=hello_with_counter._handler
+        )
+
+        TableViewer(
+            self, 'ViewHitCounter',
+            title='Hello Hits',
+            table=hello_with_counter.table,
+        )
